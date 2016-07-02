19ae79d11eccc0fbb162a074d57306c359eeea28
Santosh Purbey <santosh.purbey@studentpartner.com>
2016-07-02 11:08:54 +0000

    adding static and media urlpatterns in urls.py

--- dyf_ngo/urls.py
+++ dyf_ngo/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
